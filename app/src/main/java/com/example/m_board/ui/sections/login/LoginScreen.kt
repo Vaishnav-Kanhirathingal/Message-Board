7120d022e4d1225926f6fa909cd33e27e69bb3bf
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-07-09 22:17:17 +0530

    login screen changes

--- app/src/main/java/com/example/m_board/ui/sections/login/LoginScreen.kt
+++ app/src/main/java/com/example/m_board/ui/sections/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.m_board.ui.sections.login
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,14 +9,24 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.GetCredentialCancellationException
 import com.example.m_board.util.CustomSharedValues.setSizeLimitation
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import kotlinx.coroutines.launch
 
 object LoginScreen {
     @Composable
-    fun Screen(modifier: Modifier) {
+    fun Screen(
+        modifier: Modifier,
+        loginViewModel: LoginViewModel
+    ) {
         Scaffold(
             modifier = modifier,
             content = {
@@ -30,10 +41,36 @@ object LoginScreen {
                     ),
                     content = {
                         Text(text = "Google login screen")
+                        val context = LocalContext.current
+                        val scope = rememberCoroutineScope()
                         Button(
                             modifier = Modifier.setSizeLimitation(),
                             onClick = {
-                                TODO()
+                                val googleIdOption = GetGoogleIdOption.Builder()
+                                    .setServerClientId("426664497402-65qgep1ufh52va3aqt5ighd94tkireb9.apps.googleusercontent.com") // TODO: local.prop?
+                                    .setFilterByAuthorizedAccounts(filterByAuthorizedAccounts = false)
+                                    .build()
+                                val request = GetCredentialRequest.Builder()
+                                    .addCredentialOption(googleIdOption)
+                                    .build()
+                                scope.launch {
+                                    try {
+                                        val result = CredentialManager
+                                            .create(context = context)
+                                            .getCredential(
+                                                request = request,
+                                                context = context
+                                            )
+                                        loginViewModel.login(credential = result.credential)
+                                    } catch (e: GetCredentialCancellationException) {
+                                        e.printStackTrace()
+                                    } catch (e: Exception) {
+                                        e.printStackTrace()
+                                        Toast.makeText(
+                                            context, "Something went wrong", Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
                             },
                             content = {
                                 Text(text = "Login")
--- app/src/main/java/com/example/m_board/ui/sections/login/LoginViewModel.kt
+++ app/src/main/java/com/example/m_board/ui/sections/login/LoginViewModel.kt
@@ -1,6 +1,47 @@
 package com.example.m_board.ui.sections.login
 
+import androidx.credentials.Credential
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.m_board.util.ScreenState
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class LoginViewModel : ViewModel() {
+    private val _screenState: MutableStateFlow<ScreenState<Unit>> =
+        MutableStateFlow(ScreenState.PreCall())
+    val screenState: StateFlow<ScreenState<Unit>> get() = _screenState
+
+    fun login(credential: Credential) {
+        viewModelScope.launch {
+            _screenState.value = ScreenState.Loading()
+            try {
+
+                val googleIdToken =
+                    GoogleIdTokenCredential
+                        .createFrom(data =credential.data)
+                        .idToken
+
+
+
+                FirebaseAuth.getInstance()
+                    .signInWithCredential(
+                        GoogleAuthProvider.getCredential(
+                            googleIdToken,
+                            null
+                        )
+                    )
+                    .await()
+                ScreenState.Loaded(result = Unit)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                ScreenState.ApiError.fromException<Unit>(e = e)
+            }.let { _screenState.value = it }
+        }
+    }
 }
