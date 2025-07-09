90a22bfbde12ea0cd5faffea69087565a78ca567
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-07-09 22:17:02 +0530

    screen state created

---
+++ app/src/main/java/com/example/m_board/util/ScreenState.kt
@@ -0,0 +1,53 @@
+package com.example.m_board.util
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+sealed class ScreenState<T> {
+    class PreCall<T> : ScreenState<T>()
+    class Loading<T> : ScreenState<T>()
+    class Loaded<T>(val result: T) : ScreenState<T>()
+
+    sealed class ApiError<T>(
+        val generalToastMessage: String
+    ) : ScreenState<T>() {
+        companion object {
+            fun <T> fromException(e: Exception): ApiError<T> {
+                return when (e) {
+                    is SocketTimeoutException, is UnknownHostException -> NetworkError()
+                    else -> SomethingWentWrong()
+                }
+            }
+        }
+
+        private val TAG = this::class.simpleName
+
+        private var errorHasBeenDisplayed: Boolean = false
+
+        fun manageToastActions(context: Context) {
+            if (!this.errorHasBeenDisplayed) {
+                this.errorHasBeenDisplayed = true
+                Toast.makeText(
+                    context,
+                    this.generalToastMessage,
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Log.d(TAG, "avoided displaying error multiple times")
+            }
+        }
+
+        class NetworkError<T> : ApiError<T>(
+            generalToastMessage = "No internet connection. Please check your network."
+        )
+
+        class SomethingWentWrong<T>(alternateToastMessage: String? = null) : ApiError<T>(
+            generalToastMessage = alternateToastMessage ?: "Something went wrong. Please try again."
+        )
+    }
+
+    val isLoading get() = (this is Loading)
+}
