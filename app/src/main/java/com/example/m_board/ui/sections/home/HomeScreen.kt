25582d28c8aefb77ccc447dd9695d593043285e6
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-07-09 21:16:55 +0530

    home screen files added

---
+++ app/src/main/java/com/example/m_board/ui/sections/home/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.example.m_board.ui.sections.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.m_board.ui.sections.login.LoginViewModel
+
+object HomeScreen {
+    @Composable
+    fun Screen(
+        modifier: Modifier,
+        loginViewModel: LoginViewModel
+    ) {
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/m_board/ui/sections/home/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.m_board.ui.sections.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+}
