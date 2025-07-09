c8f355925e4712b20b809ee4f051c92fea8eff1b
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-07-10 04:09:14 +0530

    text field changes

--- app/src/main/java/com/example/m_board/ui/sections/home/HomeScreen.kt
+++ app/src/main/java/com/example/m_board/ui/sections/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.m_board.ui.sections.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,29 +9,32 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -152,17 +156,32 @@ object HomeScreen {
                                     color = MaterialTheme.colorScheme.surfaceContainer,
                                     shape = RoundedCornerShape(size = 32.dp)
                                 )
-                                .padding(
-                                    horizontal = 16.dp,
-                                    vertical = 8.dp
-                                ),
+                                .padding(horizontal = 16.dp),
                             horizontalArrangement = Arrangement.spacedBy(space = 4.dp),
                             verticalAlignment = Alignment.CenterVertically,
                             content = {
                                 val text = homeViewModel.message.collectAsState().value
-                                BasicTextField(
-                                    modifier = Modifier.weight(weight = 1f),
-                                    textStyle = TextStyle(color = MaterialTheme.colorScheme.onSurfaceVariant),
+                                TextField(
+                                    modifier = Modifier
+                                        .weight(weight = 1f)
+                                        .setSizeLimitation()
+                                        .padding(horizontal = 8.dp),
+                                    colors = TextFieldDefaults.colors(
+                                        focusedContainerColor = Color.Transparent,
+                                        unfocusedContainerColor = Color.Transparent,
+                                        disabledContainerColor = Color.Transparent,
+                                        errorContainerColor = Color.Transparent,
+
+                                        focusedIndicatorColor = Color.Transparent,
+                                        unfocusedIndicatorColor = Color.Transparent,
+                                        disabledIndicatorColor = Color.Transparent,
+                                        errorIndicatorColor = Color.Transparent
+                                    ),
+//                                    textStyle = TextStyle(
+//                                        fontSize = 16.sp,
+//                                        fontWeight = FontWeight.Medium,
+//                                        color = MaterialTheme.colorScheme.onSurface
+//                                    ),
                                     value = text,
                                     onValueChange = { txt -> homeViewModel.message.value = txt },
                                     enabled = !screenState.value.isLoading
