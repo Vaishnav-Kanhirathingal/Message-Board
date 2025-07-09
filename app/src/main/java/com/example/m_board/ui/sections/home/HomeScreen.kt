6df7e94c1bd56961bb55c86235333567a55fe4fa
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-07-10 00:27:48 +0530

    home screen updated with viewmodel

--- app/src/main/java/com/example/m_board/ui/sections/home/HomeScreen.kt
+++ app/src/main/java/com/example/m_board/ui/sections/home/HomeScreen.kt
@@ -1,8 +1,32 @@
 package com.example.m_board.ui.sections.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.m_board.ui.sections.login.LoginViewModel
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.m_board.util.CustomSharedValues.setSizeLimitation
 
 object HomeScreen {
     @Composable
@@ -10,6 +34,107 @@ object HomeScreen {
         modifier: Modifier,
         homeViewModel: HomeViewModel
     ) {
-        TODO()
+        Scaffold(
+            modifier = modifier,
+            content = { paddingValues ->
+                val screenState = homeViewModel.screenState.collectAsState()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues = paddingValues),
+                    content = {
+                        val list = homeViewModel.messageList.collectAsState().value
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(weight = 1f),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Bottom,
+                            content = {
+                                items(
+                                    items = list,
+                                    itemContent = {
+                                        Column(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(horizontal = 16.dp)
+                                                .background(
+                                                    color = MaterialTheme.colorScheme.surfaceContainer,
+                                                    shape = RoundedCornerShape(size = 16.dp)
+                                                )
+                                                .padding(
+                                                    horizontal = 8.dp,
+                                                    vertical = 4.dp
+                                                ),
+                                            content = {
+                                                Text(
+                                                    modifier = Modifier.align(alignment = Alignment.Start),
+                                                    text = it.userName ?: "[Unnamed User]",
+                                                    fontWeight = FontWeight.SemiBold,
+                                                    fontSize = 12.sp,
+                                                    color = MaterialTheme.colorScheme.onSurface
+                                                )
+                                                Text(
+                                                    text = it.message,
+                                                    fontWeight = FontWeight.Medium,
+                                                    fontSize = 16.sp,
+                                                    color = MaterialTheme.colorScheme.onSurface
+                                                )
+                                                Text(
+                                                    text = it.time.toHourMinute(),
+                                                    fontWeight = FontWeight.SemiBold,
+                                                    fontSize = 12.sp,
+                                                    color = MaterialTheme.colorScheme.onSurface
+                                                )
+                                            }
+                                        )
+                                    }
+                                )
+                            }
+                        )
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 8.dp)
+                                .background(
+                                    color = MaterialTheme.colorScheme.surfaceContainer,
+                                    shape = RoundedCornerShape(size = 32.dp)
+                                )
+                                .padding(
+                                    horizontal = 16.dp,
+                                    vertical = 8.dp
+                                ),
+                            horizontalArrangement = Arrangement.spacedBy(space = 4.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            content = {
+                                val text = homeViewModel.message.collectAsState().value
+                                BasicTextField(
+                                    modifier = Modifier.weight(weight = 1f),
+                                    textStyle = TextStyle(color = MaterialTheme.colorScheme.onSurfaceVariant),
+                                    value = text,
+                                    onValueChange = { txt -> homeViewModel.message.value = txt },
+                                    enabled = !screenState.value.isLoading
+                                )
+                                IconButton(
+                                    modifier = Modifier.setSizeLimitation(),
+                                    enabled = !screenState.value.isLoading,
+                                    onClick = {
+                                        text.takeUnless { it.isBlank() }
+                                            ?.let { homeViewModel.sendMessage(message = it) }
+                                    },
+                                    content = {
+                                        Icon(
+                                            imageVector = Icons.AutoMirrored.Filled.Send,
+                                            contentDescription = null
+                                        )
+                                    }
+                                )
+                            }
+                        )
+                    }
+                )
+            }
+        )
     }
 }
--- app/src/main/java/com/example/m_board/ui/sections/home/HomeViewModel.kt
+++ app/src/main/java/com/example/m_board/ui/sections/home/HomeViewModel.kt
@@ -1,6 +1,94 @@
 package com.example.m_board.ui.sections.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.m_board.util.ScreenState
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class HomeViewModel : ViewModel() {
+    private val TAG = this::class.simpleName
+
+    companion object {
+        const val BOARD_REFERENCE = "BOARD"
+    }
+
+    val message: MutableStateFlow<String> = MutableStateFlow("")
+
+    private val _messageList: MutableStateFlow<List<Message>> = MutableStateFlow(listOf())
+    val messageList: StateFlow<List<Message>> get() = _messageList
+
+    private val _screenState: MutableStateFlow<ScreenState<Unit>> =
+        MutableStateFlow(ScreenState.PreCall())
+    val screenState: StateFlow<ScreenState<Unit>> get() = _screenState
+
+    fun sendMessage(message: String) {
+        _screenState.value = ScreenState.Loading()
+        viewModelScope.launch {
+            try {
+                Log.d(TAG,"api call starting")
+                Firebase
+                    .database
+                    .getReference(BOARD_REFERENCE)
+                    .push()
+                    .setValue(
+                        Message(
+                            time = System.currentTimeMillis(),
+                            message = message,
+                            userName = null
+                        )
+                    ).await()
+                ScreenState.Loaded(result = Unit)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                ScreenState.ApiError.fromException(e = e)
+            }.let { _screenState.value = it }
+        }
+    }
+
+    init {
+        liveListener()
+    }
+
+    fun liveListener() {
+        Firebase.database
+            .getReference(BOARD_REFERENCE)
+            .orderByChild(Message::time.name)
+            .addValueEventListener(
+                object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        _messageList.value = snapshot.children.mapNotNull { child ->
+                            child.getValue(Message::class.java)
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        error.toException().printStackTrace()
+                    }
+                }
+            )
+    }
+}
+
+data class Message(
+    val time: Long,
+    val message: String,
+    val userName: String?
+)
+
+fun Long.toHourMinute(): String {
+    return Instant.ofEpochMilli(this)
+        .atZone(ZoneId.systemDefault())
+        .format(DateTimeFormatter.ofPattern("hh:mm"))
 }
